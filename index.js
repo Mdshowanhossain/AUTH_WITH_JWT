1db1eb19c3f607e36ef898b2b653a95c3d138e4f
Mdshowanhossain <76901439+Mdshowanhossain@users.noreply.github.com>
2021-09-30 21:47:41 +0600

    Bug_Fix

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userHandler = require('./routeHandler/userHandler');
-const todoHandler = require('./routeHandler/todoHandler');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./db/db')
+
+const blog = require('./routes/blog');
+const Registration = require('./routes/RegLog');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 5000
 
 const app = express();
-dotenv.config();
+
+
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connection successful'))
-    .catch(err => console.log(err));
+app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/todo', todoHandler);
+app.use(bodyParser());
+app.use(cors());
 
-app.get('/ame', (req, res) => {
-    res.send('i am home')
+// <========DATABASE CONNECTION========>
+connectDB();
+
+// <===========ROUTER ===========>
+app.use('/blog', blog);
+app.use('/', Registration)
+
+app.listen(PORT, () => {
+    console.log('Your Server is Running at', PORT)
 })
-
-app.use('/users', userHandler);
-
-const errorHandler = (err, req, res, next) => {
-    if (res.headerSent) {
-        return next(err);
-    } else {
-        res.status(500).json({ error: err })
-    }
-}
-app.use(errorHandler);
-
-app.listen(4000, () => {
-    console.log(`Your server is running at 4000`)
-});
-
-
-
-
